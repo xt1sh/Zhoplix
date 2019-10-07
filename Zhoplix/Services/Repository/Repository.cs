9b76c1847f91505ac31e497c0c68b23b1c008133
xt1sh <azylav@gmail.com>
2019-10-07 21:22:20 +0300

    Full repository

--- Zhoplix/Services/Repository/Repository.cs
+++ Zhoplix/Services/Repository/Repository.cs
@@ -21,9 +21,68 @@ namespace Zhoplix.Services
         public async Task<T> GetObjectByIdAsync(int id) =>
             await _context.FindAsync<T>(id);
 
+        public async Task<T> GetObjectByInstanceAsync(T obj) =>
+            await _context.FindAsync<T>(obj);
+
         public async Task AddObjectAsync(T model)
         {
             await _context.AddAsync(model);
+            await SaveAllAsync();
+        }
+
+        public async Task AddRangeAsync(T[] objs)
+        {
+            await _context.AddRangeAsync(objs);
+        }
+
+        public async Task ChangeObjectAsync(T newObj)
+        {
+            _context.Update(newObj);
+            await SaveAllAsync();
+        }
+
+        public async Task ChangeRangeAsync(T[] newObjs)
+        {
+            _context.UpdateRange(newObjs);
+            await SaveAllAsync();
+        }
+
+        public async Task ChangeObjectByIdAsync(int id, T newObj)
+        {
+            try
+            {
+                var obj = await GetObjectByIdAsync(id);
+                newObj.GetType().GetProperty("Id").SetValue(obj, id);
+                await ChangeObjectAsync(newObj);
+            }
+            catch(Exception e)
+            {
+                _logger.LogError($"ChangeObjectByIdAsync error: {e.Message}");
+            }
+        }
+
+        public async Task DeleteObjectAsync(T obj)
+        {
+            _context.Remove(obj);
+            await SaveAllAsync();
+        }
+
+        public async Task DeleteObjectByIdAsync(int id)
+        {
+            _context.Remove(await GetObjectByIdAsync(id));
+            await SaveAllAsync();
+        }
+
+        public async Task DeleteRangeAsync(T[] objs)
+        {
+            _context.RemoveRange(objs);
+            await SaveAllAsync();
+        }
+
+        public async Task DeleteRangeByIdsAsync(int[] ids)
+        {
+            var objectsToRemove = ids.Select(async id => await GetObjectByIdAsync(id));
+            _context.RemoveRange(objectsToRemove);
             await SaveAllAsync();
         }
 
