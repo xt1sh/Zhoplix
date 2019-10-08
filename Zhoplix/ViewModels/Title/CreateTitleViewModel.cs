cc5de328b07e2716cc4967643da17d0fbfc2416e
xt1sh <azylav@gmail.com>
2019-10-08 21:17:32 +0300

    added first try of admin panel

--- Zhoplix/ViewModels/Title/CreateTitleViewModel.cs
+++ Zhoplix/ViewModels/Title/CreateTitleViewModel.cs
@@ -11,8 +11,7 @@ namespace Zhoplix.ViewModels.Title
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public float Rating { get; set; }
-        public int AgeRestriction { get; set; }
+            public int AgeRestriction { get; set; }
         public List<CreateSeasonViewModel> Seasons { get; set; }
     }
 }
