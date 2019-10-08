8d9e43fbb89cd407068099c3fcb2ed541cf6ab89
xt1sh <azylav@gmail.com>
2019-10-08 22:52:55 +0300

    added models

--- Zhoplix/ClientApp/src/app/components/admin/admin.component.ts
+++ Zhoplix/ClientApp/src/app/components/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { CreateTitle } from 'src/app/models/createTitle';
 
 @Component({
   selector: 'app-admin',
@@ -7,44 +8,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+
   title: CreateTitle;
 
   constructor() { }
 
   ngOnInit() {
-    this.title = {
-      name: '',
-      description: '',
-      ageRestriction: 0,
-      seasons: [{
-          name: '',
-          episodes: [{
-            name: '',
-            video: ''
-          }]
-        }
-      ]
-    }
+    this.title = new CreateTitle();
   }
-
-  onClick() {
-    console.log(this.title.name);
-  }
-}
-
-export interface CreateTitle {
-  name: string;
-  description: string;
-  ageRestriction: number;
-  seasons: CreateSeason[];
-}
-
-export interface CreateSeason {
-  name: string;
-  episodes: CreateEpisode[];
-}
-
-export interface CreateEpisode {
-  name: string;
-  video: any;
 }
---
+++ Zhoplix/ClientApp/src/app/models/createSeason.ts
@@ -0,0 +1,6 @@
+import { CreateEpisode } from "./createEpisode";
+
+export class CreateSeason {
+  name: string = '';
+  episodes: Array<CreateEpisode> = new Array<CreateEpisode>();
+}
