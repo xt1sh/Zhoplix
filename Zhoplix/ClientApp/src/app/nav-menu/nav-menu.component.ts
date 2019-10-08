d53988cd89dbc3819195c827446a41850060dfcd
Ivan Koval <azylav@gmail.com>
2019-10-08 13:30:22 +0300

    main page changes

--- Zhoplix/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Zhoplix/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+  zhoplixLogoSrc = 'Logos/zhoplix_empty.png';
   isExpanded = false;
 
   collapse() {
