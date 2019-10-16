f50d8e091c52923daa96292e0800dbad58e93719
xt1sh <azylav@gmail.com>
2019-10-16 20:45:52 +0300

    id for registration

--- Zhoplix/ClientApp/src/app/components/authentication/registration/registration.component.ts
+++ Zhoplix/ClientApp/src/app/components/authentication/registration/registration.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit, AfterViewInit, ElementRef, Renderer2, ViewChild } fr
 import { SlideComponent, CarouselComponent } from 'angular-bootstrap-md';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { Router, NavigationEnd, ActivatedRoute, RoutesRecognized } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent implements AfterViewInit {
+export class RegistrationComponent implements AfterViewInit, OnInit {
+
+  currentSlide: number;
 
   @ViewChild(CarouselComponent, {static: true}) item: CarouselComponent;
   firstLogo = 'tempLogos/firstLogo.png';
@@ -18,17 +21,32 @@ export class RegistrationComponent implements AfterViewInit {
     password: [undefined, Validators.required],
   });
 
-  constructor(private rd: Renderer2, private formBuilder: FormBuilder, private auth: AuthenticationService) { }
+  constructor(private rd: Renderer2,
+              private formBuilder: FormBuilder,
+              private auth: AuthenticationService,
+              private readonly router: Router,
+              private readonly route: ActivatedRoute) { }
 
-  onClickOne() {
-      this.item.nextSlide();
+  ngOnInit() {
+    this.currentSlide = 0;
+    this.router.navigate(['signup/1']);
+    this.router.events.subscribe(val => {
+      if(val instanceof RoutesRecognized)
+        if(+val.state.root.firstChild.params["id"] === this.currentSlide)
+          this.item.nextSlide();
+    });
+  }
+
+  onClickOne(next: number) {
+    this.router.navigate([`signup/${next}`]);
+    this.currentSlide = next;
   }
 
   onSubmit() {
     this.auth.signUp(this.registrationForm.value)
     .subscribe(res => {
       this.auth.setToken(res);
-      
+
     })
   }
 
