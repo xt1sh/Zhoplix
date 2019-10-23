35af3a95ab2a0acd013766b124e35990e6c4134d
Ivan Koval <azylav@gmail.com>
2019-10-23 22:23:19 +0300

    no signin button in /login

--- Zhoplix/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
+++ Zhoplix/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,35 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 const routes: string[] = [];
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  selector: "app-nav-menu",
+  templateUrl: "./nav-menu.component.html",
+  styleUrls: ["./nav-menu.component.css"]
 })
 export class NavMenuComponent implements OnInit {
-  zhoplixLogoSrcLarge = 'Logos/zhoplix_empty_167.png';
-  zhoplixLogoSrcMedium = 'Logos/zhoplix_empty_134.png';
-  zhoplixLogoSrcSmall = 'Logos/zhoplix_empty_108.png';
+  zhoplixLogoSrcLarge = "Logos/zhoplix_empty_167.png";
+  zhoplixLogoSrcMedium = "Logos/zhoplix_empty_134.png";
+  zhoplixLogoSrcSmall = "Logos/zhoplix_empty_108.png";
   isExpanded = false;
   toShow$: Observable<boolean>;
   toShow: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  toShowSignIn$: Observable<boolean>;
+  toShowSignIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor(private readonly router: Router) {
-    router.events.subscribe(() => {
-      this.toShow$ = this.getToShowValue;
-    })
-  }
+  constructor(private readonly router: Router) {}
 
   ngOnInit() {
     this.toShow$ = this.getToShowValue;
+    this.toShowSignIn$ = this.getToShowSignInValue;
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.toShow$ = this.getToShowValue;
+        this.toShowSignIn$ = this.getToShowSignInValue;
+      });
   }
 
   get getToShowValue() {
     this.toShow.next(!routes.includes(this.router.url.slice(1)));
     return this.toShow.asObservable();
+  }
+
+  get getToShowSignInValue() {
+    this.toShowSignIn.next(!this.router.url.includes("login"));
+    return this.toShowSignIn.asObservable();
   }
 
   collapse() {
