9e1a46549cf606a45cd59b17a520a255f39f56e8
Ivan Koval <azylav@gmail.com>
2019-10-28 15:32:45 +0200

    added fingerprint

--- Zhoplix/ClientApp/src/app/components/home/home.component.ts
+++ Zhoplix/ClientApp/src/app/components/home/home.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private readonly auth: AuthenticationService) {}
+
+  ngOnInit() {
+    this.auth.createFingerprint();
+  }
 }
