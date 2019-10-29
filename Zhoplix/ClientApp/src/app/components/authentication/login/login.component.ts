7b20a29ebb8e57158dba05b998cb3117105562e2
xt1sh <azylav@gmail.com>
2019-10-29 15:46:58 +0200

    bug fixes

--- Zhoplix/ClientApp/src/app/components/authentication/login/login.component.ts
+++ Zhoplix/ClientApp/src/app/components/authentication/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Login } from 'src/app/models/login';
 
 
 @Component({
@@ -24,7 +25,8 @@ export class LoginComponent implements OnInit {
   constructor(private formBuilder: FormBuilder,
      private auth: AuthenticationService,
      private router: Router,
-     private route: ActivatedRoute) { }
+     private route: ActivatedRoute,
+     private readonly ngZone: NgZone) { }
 
   ngOnInit() {
     if(this.auth.isLoggedIn) {
@@ -40,17 +42,25 @@ export class LoginComponent implements OnInit {
     this.loginSpinner = true;
     let password = this.loginForm.controls['password'];
     if (this.loginForm.valid) {
-    this.auth.login(this.loginForm.value)
-        .subscribe(res => {
-          this.incorrect = false;
-          this.auth.setToken(res, this.loginForm.controls['rememberMe'].value);
-          this.router.navigate([this.returnUrl]);
-          this.loginSpinner = false;
-        }, error => {
-          this.incorrect = true;
-          this.loginSpinner = false;
-          password.reset();
-        });
+      let login: Login = this.loginForm.value;
+      const loginObservable = this.auth.createFingerprint().subscribe(value => {
+        login.fingerPrint = value;
+        this.auth.login(login)
+          .subscribe(res => {
+            console.log(1)
+            this.incorrect = false;
+            this.auth.setToken(res, this.loginForm.controls['rememberMe'].value);
+            this.ngZone.run(() => this.router.navigate([this.returnUrl])).then();
+            this.loginSpinner = false;
+            console.log(this.loginSpinner)
+            loginObservable.unsubscribe();
+          }, error => {
+            this.incorrect = true;
+            this.loginSpinner = false;
+            password.reset();
+            loginObservable.unsubscribe();
+          });
+      })
     }
     else {
       this.loginSpinner = false;
