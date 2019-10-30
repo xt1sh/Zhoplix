8d3ed203ec56e6b7f101f0a877652f7e7f6cbe10
xt1sh <azylav@gmail.com>
2019-10-31 00:03:03 +0200

    fix fingerprint

--- Zhoplix/ClientApp/src/app/components/authentication/confirmEmail/confirmEmail.component.ts
+++ Zhoplix/ClientApp/src/app/components/authentication/confirmEmail/confirmEmail.component.ts
@@ -22,10 +22,12 @@ export class ConfirmEmailComponent implements OnInit {
 }
 
   ngOnInit() {
-    this.auth.confirmEmail(this.userId, this.token, this.auth.fingerPrint)
-      .subscribe(res => {
-        this.auth.setTokens(res.body as Tokens);
-      });
-    this.router.navigate(['']);
+    this.auth.createFingerprint().subscribe(value => {
+      this.auth.confirmEmail(this.userId, this.token, value)
+        .subscribe(res => {
+          this.auth.setTokens(res.body as Tokens);
+        });
+      this.router.navigate(['']);
+    })
   }
 }
